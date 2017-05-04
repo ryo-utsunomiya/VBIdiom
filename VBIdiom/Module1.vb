adc869f730446b00d20d02d6c82966558946abf5
Ryo Utsunomiya <millionsnowliving@gmail.com>
2017-05-04 17:33:56 +0900

    Excesice 1.2

--- VBIdiom/Module1.vb
+++ VBIdiom/Module1.vb
@@ -1,11 +1,22 @@
 ﻿Imports System.Console
-Imports VBIdiom.Commodity
 
 Module Module1
 
     Sub Main()
-        Dim d = New Product(code:=98, name:="どら焼き", price:=210)
-        WriteLine(d.GetTax())
+        Dim c = New MyKlass() With {.X = 10, .Y = 20}
+        Dim s = New MyStruct() With {.X = 5, .Y = 40}
+        PrintObjects(c, s)
+        WriteLine($"X: {c.X} Y: {c.Y}")
+        WriteLine($"X: {s.X} Y: {s.Y}")
+    End Sub
+
+    Sub PrintObjects(c As MyKlass, s As MyStruct)
+        c.X *= 2
+        c.Y *= 2
+        s.X *= 2
+        s.Y *= 2
+        WriteLine($"X: {c.X} Y: {c.Y}")
+        WriteLine($"X: {s.X} Y: {s.Y}")
     End Sub
 
 End Module
---
+++ VBIdiom/MyKlass.vb
@@ -0,0 +1,9 @@
+﻿Public Class MyKlass
+    Public Property X As Integer
+    Public Property Y As Integer
+End Class
+
+Public Structure MyStruct
+    Public Property X As Integer
+    Public Property Y As Integer
+End Structure
