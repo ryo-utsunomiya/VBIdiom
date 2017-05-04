0df70e02a026fdb14555f2268cf9323d460ac794
Ryo Utsunomiya <millionsnowliving@gmail.com>
2017-05-04 17:15:27 +0900

    Exercise 1.1

---
+++ VBIdiom/Commodity/Product.vb
@@ -0,0 +1,27 @@
+﻿Namespace Commodity
+
+    Public Class Product
+        Public ReadOnly Property Code As Integer
+        Public ReadOnly Property Name As String
+        Public ReadOnly Property Price As Integer
+
+        Sub New(code As Integer, name As String, price As Integer)
+            Me.Code = code
+            Me.Name = name
+            Me.Price = price
+        End Sub
+
+        ''' <summary>
+        ''' 消費税額を求める
+        ''' </summary>
+        ''' <returns></returns>
+        Public Function GetTax() As Integer
+            Return CInt(Price * 0.08)
+        End Function
+
+        Public Function GetPriceIncludingTax() As Integer
+            Return Price + GetTax()
+        End Function
+    End Class
+
+End Namespace
--- VBIdiom/Module1.vb
+++ VBIdiom/Module1.vb
@@ -1,7 +1,11 @@
-﻿Module Module1
+﻿Imports System.Console
+Imports VBIdiom.Commodity
+
+Module Module1
 
     Sub Main()
-
+        Dim d = New Product(code:=98, name:="どら焼き", price:=210)
+        WriteLine(d.GetTax())
     End Sub
 
 End Module
